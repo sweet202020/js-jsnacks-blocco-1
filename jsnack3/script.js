255b9b2991f0235c86d89eae57ed2020cadcf601
sweet202020 <dolcealessio1@gmail.com>
2022-10-12 12:49:21 +0200

    problem snack3

---
+++ jsnack3/script.js
@@ -0,0 +1,11 @@
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+let somma ;
+for (i=0; i<10; i++){
+    const num= Number(prompt('inserisci un numero'));
+    somma += num 
+    
+}
+
+console.log(somma)
+
+//Il programma stampa la somma di tutti i numeri inseriti. 
