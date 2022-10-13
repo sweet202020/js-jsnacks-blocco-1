57f77287edf2dd49de18a5cea2ff84bc923ee75e
sweet202020 <dolcealessio1@gmail.com>
2022-10-13 12:04:32 +0200

    final snack 3 with loop while

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,11 +1,29 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-let somma ;
+/* let somma = 0;
 for (i=0; i<10; i++){
     const num= Number(prompt('inserisci un numero'));
-    somma += num 
+    (somma += num)  
     
 }
 
-console.log(somma)
+console.log(somma) */
 
 //Il programma stampa la somma di tutti i numeri inseriti. 
+
+//utilizzo il ciclo while
+/* 
+//contatore
+while(condizione){
+
+    //istruzione per bloccare il ciclo
+}
+*/
+
+let somma=0;
+i=0;
+while (i<10){
+    const num = Number(prompt('inserisci un numero'));
+    (somma+=num);
+    i++//istruzione che blocca il ciclo
+}
+console.log(somma)
